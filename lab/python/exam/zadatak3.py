3491adf0c36f4767505af2b44a12debd3b6545eb
Domagoj Maric <domamaric20@gmail.com>
2024-06-06 17:42:10 +0200

    lab/python/exam update code clarity and efficiency

--- lab/python/exam/zadatak3.py
+++ lab/python/exam/zadatak3.py
@@ -1,14 +1,16 @@
 import sys
 import pathlib
 
+from collections import defaultdict
+
 
 def print_addresses(list_of_items):
     print("--------------------------------")
-    print("  IP adrese   |  Br. pristupa")
+    print("  IP adrese   |  Br. pristupa   ")
     print("--------------------------------")
 
-    for ip, num in sorted(list_of_items, key=lambda x: x[1], reverse=True):
-        print(f"{ip:>12}     {num}")
+    for ip, num in sorted(list_of_items, key=lambda pair: pair[1], reverse=True):
+        print(f"{ip:>12}{num:>11}")
 
     print("--------------------------------")
 
@@ -18,29 +20,17 @@ if __name__ == "__main__":
         sys.exit(1)
 
     filename = pathlib.Path(sys.argv[1])
+    ip_addresses = defaultdict(int)
 
     with open(filename, "r") as f:
-        lines = [x.strip() for x in f.readlines()]
+        for line in f:
+            parts = line.strip().split()
 
-    ip_addr = {}
+            addresses = parts[0].split(".")
+            subnet = f"{addresses[0]}.{addresses[1]}.*.*"
+            no_of_bytes = parts[-1]
 
-    for line in lines:
-        parts = line.split()
-        
-        ip = parts[0]
-        addresses = ip.split(".")
-        subnet = addresses[0] + "." + addresses[1] + ".*.*"
-        no_of_bytes = parts[-1]
+            if no_of_bytes.isdigit():  # Is not '-'
+                ip_addresses[subnet] += int(no_of_bytes)
 
-        if no_of_bytes != "-":
-            if subnet in ip_addr:
-                ip_addr[subnet] += int(no_of_bytes)
-            else:
-                ip_addr[subnet] = int(no_of_bytes)
-   
-    list_of_items = []
-
-    for elem in ip_addr:
-        list_of_items.append((elem, ip_addr[elem]))
-
-    print_addresses(list_of_items)
+    print_addresses(list(ip_addresses.items()))
